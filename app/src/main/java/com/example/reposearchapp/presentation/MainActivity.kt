7bb8d869113b4db4f83ca748be441c1da3e63f33
Yeoonju.jin <aurora32s@naver.com>
2022-07-12 16:57:23 +0900

    feat: HomeFragment 생성 및 MainActivity와 연결

--- app/src/main/java/com/example/reposearchapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/reposearchapp/presentation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.reposearchapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
+import com.example.reposearchapp.R
 import com.example.reposearchapp.data.Result
 import com.example.reposearchapp.data.remote.RepoService
 import com.example.reposearchapp.data.safeApiCall
 import com.example.reposearchapp.databinding.ActivityMainBinding
+import com.example.reposearchapp.presentation.home.HomeFragment
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -31,9 +34,14 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        initViews()
     }
 
     private fun initViews() = with(binding) {
-        
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragmentContainer, HomeFragment())
+        }
     }
 }
---
+++ app/src/main/java/com/example/reposearchapp/presentation/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.reposearchapp.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.reposearchapp.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
