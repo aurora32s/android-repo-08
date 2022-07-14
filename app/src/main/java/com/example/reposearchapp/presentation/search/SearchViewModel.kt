fc826fc2b062ac9e200c1be10fd1ec190cf26501
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-07-14 15:53:09 +0900

    feat : 검색 로직 구현 #11

---
+++ app/src/main/java/com/example/reposearchapp/presentation/search/SearchViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.reposearchapp.presentation.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.reposearchapp.data.RepoItemModel
+import com.example.reposearchapp.data.Result
+import com.example.reposearchapp.data.entity.toModel
+import com.example.reposearchapp.data.remote.GithubService
+import com.example.reposearchapp.data.safeApiCall
+import com.example.reposearchapp.util.GithubLanguageColorUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(private val color: GithubLanguageColorUtil) : ViewModel() {
+    private var searchJob: Job? = null
+    var lastQuery = ""
+
+    private val _repoList = MutableLiveData<List<RepoItemModel>>()
+    val repoList: LiveData<List<RepoItemModel>> get() = _repoList
+
+    init {
+        viewModelScope.launch {
+            color.parseJson()
+        }
+    }
+
+    fun search(query: String) {
+        searchJob?.cancel()
+
+        lastQuery = query
+        searchJob = viewModelScope.launch {
+            delay(DEBOUNCE_TIME)
+            when (val result = safeApiCall { GithubService.api.getRepos(query) }) {
+                is Result.Success -> {
+                    _repoList.value =
+                        result.data.items.map { it.toModel(color.colorMap?.get(it.language)) }
+                }
+                is Result.Error -> {
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val DEBOUNCE_TIME = 300L
+    }
+}
