b1940d4eff7a2fb0441e76e6f9baec92f988ff83
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-07-17 19:21:03 +0900

    fix : 키보드 올리는 함수 수정 #25

--- app/src/main/java/com/example/reposearchapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/reposearchapp/presentation/search/SearchFragment.kt
@@ -4,16 +4,18 @@ package com.example.reposearchapp.presentation.search
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.doOnLayout
 import androidx.fragment.app.activityViewModels
 import com.example.reposearchapp.R
 import com.example.reposearchapp.databinding.FragmentSearchBinding
 import com.example.reposearchapp.presentation.adapter.RepoAdapter
 import com.example.reposearchapp.presentation.base.BaseFragment
+import com.example.reposearchapp.util.KeyboardUtil
 
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private val searchViewModel by activityViewModels<SearchViewModel>()
-    lateinit var adapter: RepoAdapter
+    private lateinit var adapter: RepoAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,9 +27,12 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
     }
 
     private fun initSearchView() {
+        binding.searchView.doOnLayout {
+            it.requestFocus()
+            KeyboardUtil.show(it.findFocus())
+        }
+
         binding.searchView.run {
-            requestFocus()
-            setQuery(searchViewModel.lastQuery, false)
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     return true
@@ -50,7 +55,21 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(LAST_QUERY, binding.searchView.query.toString())
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        val query = savedInstanceState?.getString(LAST_QUERY)
+        binding.searchView.setQuery(query, false)
+    }
+
     companion object {
         const val TAG = "SearchFragment"
+        const val LAST_QUERY = "LAST_QUERY"
     }
 }
