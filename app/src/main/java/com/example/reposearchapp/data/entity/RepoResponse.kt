6a1d03846300b25db8067d109048a344f46e3901
Haemin-Park <phm9368@sookmyung.ac.kr>
2022-07-17 15:40:58 +0900

    fix : 카멜 케이스로 수정 #25

--- app/src/main/java/com/example/reposearchapp/data/entity/RepoResponse.kt
+++ app/src/main/java/com/example/reposearchapp/data/entity/RepoResponse.kt
@@ -1,9 +1,11 @@
 package com.example.reposearchapp.data.entity
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RepoResponse(
-    val total_count: Int,
+    @SerialName("total_count")
+    val totalCount: Int,
     val items: List<Item>
 )
